74bd3b640683db59da18238b13e50fa985f2f4b8
EltunH <eltun.huseynov17@gmail.com>
2025-04-15 22:44:26 +0400

    change footer

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 const openClose = document.getElementById('openClose')
 const hiddenSideDiv = document.getElementById('hiddenSideDiv')
 const xMark = document.getElementById('xMark')
+const ac = document.getElementById('ac')
 
 function openSideDiv() {
     openClose.classList.toggle('right-[0px!important]')
     hiddenSideDiv.classList.toggle('hidden')
     xMark.classList.toggle('fa-xmark')
+}
+
+function accordionFooter(arg, svg) {
+    const elm = document.getElementById(`${arg}`)
+    const svgId = document.getElementById(`${svg}`)
+    elm.classList.toggle('max-h-[500px!important]')
+    svgId.classList.toggle('rotate-[180deg]')
 }
